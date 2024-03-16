c4419b29eb957a69ab80921e0703e17122fa1b08
Davide Mendolia <davide@gokarumi.com>
2024-03-16 11:50:06 +0100

    move to firebase hosting

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,9 +7,9 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-import * as functions from 'firebase-functions';
-import express from 'express';
-import admin from 'firebase-admin';
+import * as functions from "firebase-functions";
+import express from "express";
+import admin from "firebase-admin";
 
 admin.initializeApp();
 
@@ -18,15 +18,21 @@ const db = admin.firestore();
 
 app.use(express.json());
 
-app.get('/lastChanged', async (req: express.Request, res: express.Response) => {
-  const snapshot = await db.collection('litterChanges').doc('lastChange').get();
-  res.json({ lastChanged: snapshot.data()?.date || null });
+app.get("/lastChanged", async (
+  req: express.Request,
+  res: express.Response) => {
+  const snapshot = await db.collection("litterChanges").doc("lastChange").get();
+  res.json({lastChanged: snapshot.data()?.date || null});
 });
 
-app.post('/changeLitter', async (req: express.Request, res: express.Response) => {
+app.post("/changeLitter", async (
+  req: express.Request,
+  res: express.Response) => {
   const lastChanged: Date = new Date();
-  await db.collection('litterChanges').doc('lastChange').set({ date: lastChanged });
-  res.json({ success: true, lastChanged });
+  await db.collection("litterChanges")
+    .doc("lastChange")
+    .set({date: lastChanged});
+  res.json({success: true, lastChanged});
 });
 
 exports.app = functions.https.onRequest(app);
