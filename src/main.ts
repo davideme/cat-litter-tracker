4db8116b8ec0e0cef7badf2428dc581d13e5c460
Davide Mendolia <davide@gokarumi.com>
2024-03-17 17:17:27 +0100

    Add loader

--- src/main.ts
+++ src/main.ts
@@ -14,21 +14,32 @@ const changeLitter = httpsCallable<ChangeLitterData, ChangeLitterResponse>(funct
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <h1>Cat Litter Tracker</h1>
   <button id="changeLitter">Change Litter</button>
+  <div id="loader" style="display: none;">Loading...</div>
   <p>Last changed: <span id="lastChanged">Not yet changed</span></p>
 `
 
 setupChangeLitter(
   document.querySelector<HTMLButtonElement>('#changeLitter')!, 
-  document.querySelector<HTMLSpanElement>('#lastChanged')!)
+  document.querySelector<HTMLSpanElement>('#lastChanged')!,
+  document.querySelector<HTMLDivElement>('#loader')!)
 
 async function setLastChanged(element: HTMLSpanElement) {
   const result = await (await fetch('https://us-central1-cat-litter-tracker-app.cloudfunctions.net/app/lastChanged')).json();
   element.textContent = result.data.lastChanged ? new Date(result.data.lastChanged).toLocaleString() : 'Not yet changed';
 }
 
-function setupChangeLitter(element: HTMLButtonElement, lastChangedElement: HTMLSpanElement) {  
-  element.addEventListener('click', async () => {
-    await changeLitter();
-    await setLastChanged(lastChangedElement);
+function setupChangeLitter(buttonElement: HTMLButtonElement, lastChangedElement: HTMLSpanElement, loaderElement: HTMLDivElement) {  
+  buttonElement.addEventListener('click', async () => {
+    buttonElement.disabled = true;
+    loaderElement.style.display = 'block';
+    try {
+      await changeLitter();
+      await setLastChanged(lastChangedElement);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      buttonElement.disabled = false;
+      loaderElement.style.display = 'none';
+    }
   })
 }
