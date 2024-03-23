b8ca276d628b0953e8c3e85047f710c4b2bcd17f
Davide Mendolia <davide@gokarumi.com>
2024-03-23 13:10:14 +0100

    adding list of cats

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -15,14 +15,14 @@ import {
   ChangeLitterResponse,
   LastChangedResponse,
 } from "../../shared/functions";
-import {Timestamp} from "firebase-admin/firestore";
+import { Timestamp } from "firebase-admin/firestore";
 
 admin.initializeApp();
 
 const app = express();
 
 // Apply CORS middleware to the express app
-app.use(cors({origin: true}));
+app.use(cors({ origin: true }));
 
 // Handle preflight requests
 app.options("*", cors());
@@ -33,37 +33,42 @@ const db = admin.firestore();
 
 app.get("/lastChanged", async (
   req: express.Request,
-  res: express.Response<{data: LastChangedResponse}>) => {
+  res: express.Response<{ data: LastChangedResponse }>) => {
   const snapshot = await db.collection("litterChanges").doc("lastChange").get();
-  const lastChangedTimestamp:Timestamp = snapshot.data()?.date || null;
-  const lastChangedDate = lastChangedTimestamp.toDate() || null;
+  const lastChangedTimestamp: Timestamp = snapshot.data()?.date || null;
+  const lastChangedDate = lastChangedTimestamp?.toDate() || null;
 
   const response: LastChangedResponse = {
     lastChanged: lastChangedDate,
   };
-  res.json({data: response});
+  res.json({ data: response });
 });
 
 app.post("/changeLitter", async (
   req: express.Request,
-  res: express.Response<{data: ChangeLitterResponse | string}>) => {
+  res: express.Response<{ data: ChangeLitterResponse | string }>) => {
   const idToken = req.headers.authorization?.split("Bearer ")[1] || "";
   try {
     const decodedToken = await admin.auth().verifyIdToken(idToken);
     const userId = decodedToken.uid;
     console.log(`User ${userId} has changed the litter`);
     const lastChanged: Date = new Date();
-    await db.collection("litterChanges")
-      .doc("lastChange")
-      .set({date: lastChanged});
+
+    // Update the last litter change for each cat in the household
+    const cats = req.body.cats; // Assuming the request body contains an array of cat IDs
+    for (const catId of cats) {
+      await db.collection("litterChanges")
+        .doc(catId)
+        .set({ date: lastChanged });
+    }
 
     const response: ChangeLitterResponse = {
       success: true,
       lastChanged,
     };
-    res.json({data: response});
+    res.json({ data: response });
   } catch (error) {
-    res.status(401).send({data: "Unauthorized"});
+    res.status(401).send({ data: "Unauthorized" });
   }
 });
 
