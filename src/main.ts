10fd8ee71fe04f4e2696a925a0f398368f30cfd9
Davide Mendolia <davide@gokarumi.com>
2024-03-24 23:19:08 +0100

    add households management

--- src/main.ts
+++ src/main.ts
@@ -8,12 +8,16 @@ import { ChangeLitterData, ChangeLitterResponse } from "../shared/functions";
 import firebase from "firebase/compat/app";
 import * as firebaseui from "firebaseui";
 import { getAuth, onAuthStateChanged, setPersistence, browserLocalPersistence, User, connectAuthEmulator } from "firebase/auth";
+import { getFirestore, collection, doc, setDoc, connectFirestoreEmulator, getDocs, query, where } from "firebase/firestore";
 
 const app = initializeApp(firebaseConfig);
 // const analytics = getAnalytics(app);
 const functions = getFunctions(app);
 const auth = getAuth(app);
+const db = getFirestore(app);
+
 connectAuthEmulator(auth, "http://127.0.0.1:9099");
+connectFirestoreEmulator(db, '127.0.0.1', 8080);
 
 setPersistence(auth, browserLocalPersistence);
 const changeLitter = httpsCallable<ChangeLitterData, ChangeLitterResponse>(functions, 'app/changeLitter');
@@ -47,7 +51,9 @@ function onUserSignedIn(user: User) {
   console.log(user);
   document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     <h1>Cat Litter Tracker</h1>
+    <h2 id="householdName"></h2>
     <button id="changeLitter">Change Litter</button>
+    <button id="addHousehold">Add Household</button>
     <div id="loader" style="display: none;">Loading...</div>
     <p>Last changed: <span id="lastChanged">Not yet changed</span></p>
   `;
@@ -57,6 +63,19 @@ function onUserSignedIn(user: User) {
     document.querySelector<HTMLSpanElement>('#lastChanged')!,
     document.querySelector<HTMLDivElement>('#loader')!);
   setLastChanged(document.querySelector<HTMLSpanElement>('#lastChanged')!);
+
+  document.querySelector<HTMLButtonElement>('#addHousehold')!.addEventListener('click', async () => {
+    addHouseholdToFirestore({name: 'My Household', roles: {[user.uid]: 'owner'}});
+  });
+
+  fetchHouseholdName(user);
+
+  async function fetchHouseholdName(user: User) {
+    const householdName = (await fetchOwnedHousehold(user))[0].name ?? 'My household';
+    const householdNameElement = document.querySelector<HTMLHeadingElement>('#householdName')!;
+    householdNameElement.textContent = householdName;
+  }
+
 
   async function setLastChanged(element: HTMLSpanElement) {
     try {
@@ -87,4 +106,27 @@ function onUserSignedIn(user: User) {
       }
     });
   }
+}
+
+async function addHouseholdToFirestore(household: { name: string, roles: { [userId: string]: string } }) {
+  const householdsCollection = collection(db, "households");
+  const newHouseholdDoc = doc(householdsCollection);
+  try {
+    await setDoc(newHouseholdDoc, household);
+    console.log("Household added to Firestore");
+  } catch (error) {
+    console.error("Error adding household to Firestore: ", error);
+  }
+}
+
+async function fetchOwnedHousehold(user: User): Promise<{ id: string; name?: string}[]> {
+  const householdsCollection = collection(db, "households");
+  const querySnapshot = await getDocs(query(householdsCollection, where(`roles.${user.uid}`, "==", "owner")));
+  const ownedHouseholds: { id: string; name?: string }[] = [];
+  querySnapshot.forEach((doc) => {
+    const householdData = doc.data();
+    ownedHouseholds.push({ id: doc.id, ...householdData });
+  });
+  console.log("Owned households: ", ownedHouseholds);
+  return ownedHouseholds;
 }
