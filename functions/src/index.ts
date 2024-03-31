09779c7e4f0409b1f1f494981270b107df0e07e9
Davide Mendolia <davide@gokarumi.com>
2024-03-31 20:08:30 +0200

    remove unused function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -11,11 +11,6 @@ import * as functions from "firebase-functions";
 import express from "express";
 import admin from "firebase-admin";
 import cors from "cors";
-import {
-  ChangeLitterResponse,
-  LastChangedResponse,
-} from "../../shared/functions";
-import { Timestamp } from "firebase-admin/firestore";
 
 admin.initializeApp();
 
@@ -28,48 +23,5 @@ app.use(cors({ origin: true }));
 app.options("*", cors());
 
 app.use(express.json());
-
-const db = admin.firestore();
-
-app.get("/lastChanged", async (
-  req: express.Request,
-  res: express.Response<{ data: LastChangedResponse }>) => {
-  const snapshot = await db.collection("litterChanges").doc("lastChange").get();
-  const lastChangedTimestamp: Timestamp = snapshot.data()?.date || null;
-  const lastChangedDate = lastChangedTimestamp?.toDate() || null;
-
-  const response: LastChangedResponse = {
-    lastChanged: lastChangedDate,
-  };
-  res.json({ data: response });
-});
-
-app.post("/changeLitter", async (
-  req: express.Request,
-  res: express.Response<{ data: ChangeLitterResponse | string }>) => {
-  const idToken = req.headers.authorization?.split("Bearer ")[1] || "";
-  try {
-    const decodedToken = await admin.auth().verifyIdToken(idToken);
-    const userId = decodedToken.uid;
-    console.log(`User ${userId} has changed the litter`);
-    const lastChanged: Date = new Date();
-
-    // Update the last litter change for each cat in the household
-    const cats = req.body.cats; // Assuming the request body contains an array of cat IDs
-    for (const catId of cats) {
-      await db.collection("litterChanges")
-        .doc(catId)
-        .set({ date: lastChanged });
-    }
-
-    const response: ChangeLitterResponse = {
-      success: true,
-      lastChanged,
-    };
-    res.json({ data: response });
-  } catch (error) {
-    res.status(401).send({ data: "Unauthorized" });
-  }
-});
 
 exports.app = functions.https.onRequest(app);
